4d8d11453ce3e7f2f00cecd7b1c9307354606345
Courtney May <17388926@student.curtin.edu.au>
2018-02-11 17:03:04 +0800

    No more comment spam
    New comment for each identical issue is bad idea. Will now only post
    comments for similiar issues with variance.

--- log_resource.py
+++ log_resource.py
@@ -8,22 +8,30 @@ SIMILIAR_THRESHOLD = 0.7
 GITUSERNAME = "-"
 GITPASSWORD = "-"
 
-
 class LogResource(object):
     def on_post(self, req, resp):
         identifier = req.get_param('identifier', required=True)
         stacktrace = req.get_param('stacktrace', required=True)
+        labels = req.get_param('labels', default=None)
         git = Github(GITUSERNAME, GITPASSWORD)
         repo = git.get_repo(REPO_NAME,)
         isposted = False
         for issue in repo.get_issues():
-            if SequenceMatcher(None, issue.body, stacktrace).ratio() > 0.7:
+            ratio = SequenceMatcher(None, issue.body, stacktrace).ratio()
+            if ratio > 0.7:
                 isposted = True
                 # this is probably a similiar issue, so attach it as comment
-                issue.create_comment(stacktrace)
+                if ratio != 1:
+                    issue.create_comment(stacktrace)
+                if labels is not None:
+                    [issue.add_to_labels(label) for label in labels.split(",")]
+
 
         if not isposted:
             # create new issue
-            repo.create_issue(identifier, body=stacktrace, labels=["Game Error"])
+            if labels is not None:
+                repo.create_issue(identifier, body=stacktrace, labels=labels.split(","))
+            else:
+                repo.create_issue(identifier, body=stacktrace)
         resp.status = falcon.HTTP_200
         return
